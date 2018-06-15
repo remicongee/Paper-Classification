6ad937917f4674a04654520639c146e5e20edec3
RemiC <remicongee@gmail.com>
2018-06-15 19:36:18 +0800

    Upload run.py.

---
+++ run.py
@@ -0,0 +1,15 @@
+import model
+from utils import *
+
+
+# path initialization
+graph_path = 'Data/Cit-HepTh.txt'
+train_path = 'Data/train.csv'
+test_path = 'Data/test.csv'
+info_path = 'Data/node_information'
+
+# construct graph dataframe
+df = readGraph(graph_path)
+
+# TODO
+
