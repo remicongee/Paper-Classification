ce9fcc99297a7aa1452e53fc3e76af61f1cd2976
RemiC <remicongee@gmail.com>
2018-06-16 21:22:51 +0800

    Update run.py.

--- run.py
+++ run.py
@@ -1,22 +1,26 @@
 from model import Classifier
 from utils import *
+import torch
 
 
 # path initialization
-graph_path = 'Data/Cit-HepTh.txt'
+graph_cite_path = 'Data/Cit-HepTh.txt'
+graph_cited_path = 'Data/Cited-HepTh.txt'
 train_path = 'Data/train.csv'
 test_path = 'Data/test.csv'
 info_path = 'Data/node_information'
 
 # construct graph dataframe
-graph_df = readGraph(graph_path)
+graph_cite = readGraph(graph_cite_path)
+graph_cited = readGraph(graph_cited_path)
 
 # read node information
 info_df = readInfo(info_path)
 
 # read training set, make training graph
 x_train_id, y_train_label = readIdLabel(train_path)
-graph_train = graphTrain(graph_df, x_train_id)
+graph_cite_train = graphTrain(graph_cite, x_train_id)
+graph_cited_train = graphTrain(graph_cited, x_train_id)
 
 # transform label to one-hot vector
 y_train_onehot = label2onehot(y_train_label)
@@ -30,41 +34,31 @@ x_test_text = readText(info_df, x_test_id)
 
 # extract features, make graph featured
 x_train_feature, extractor = extractFeature(x_train_text)
-x_test_feature = None # extractor.transform(x_test_text)
-'''
-TODO: concavate train and test features => feature_list
-'''
-feature_list = list()
-# for training set
-graph_train_weighted = graphWeighted(graph_train, x_train_feature)
-graph_train_featured = graphFeatured(graph_train_weighted, x_train_feature)
-# for whole data
-graph_weigthed = graphWeighted(graph_df, feature_list)
-graph_featured = graphFeatured(graph_weigthed, feature_list)
+x_test_feature = extractor.transform(x_test_text)
 
 # make citation feature
 # for training set
-citing_train = None # citation condition matrix for training
-cited_train = None # idem.
-feature_train_citing = combineFeature(graph_train_featured, citing_train)
-feature_train_cited = combineFeature(graph_train_featured, cited_train)
+x_train_cite = combineFeature(graph_cite_train, x_train_id, x_train_feature)
+x_train_cited = combineFeature(graph_cited_train, x_train_id, x_train_feature)
 # for whole data
-citing = None
-cited = None
-feature_citing = combineFeature(graph_train_featured, citing)
-feature_cited = combineFeature(graph_train_featured, cited)
-feature_test_citing = list() # feature_citing[test_id]
-feature_test_cited = list() # feature_cited[test_id]
+x_test_cite = combineFeature(graph_cite, x_test_id, x_test_feature)
+x_test_cited = combineFeature(graph_cited, x_test_id, x_test_feature)
 
 # initialization
-feature_size = 10
+feature_size = len(x_train_cite[0])
 clf_feat = Classifier(input_size=feature_size, mode='feature')
-clf_citing = Classifier(input_size=feature_size, mode='citing')
+clf_cite = Classifier(input_size=feature_size, mode='cite')
 clf_cited = Classifier(input_size=feature_size, mode='cited')
 clf = Classifier(input_size=feature_size, mode='total',
-                 clf_feat=clf_feat, clf_citing=clf_citing, clf_cited=clf_cited)
+                 clf_feat=clf_feat, clf_cite=clf_cite, clf_cited=clf_cited)
 
-# TODO: train
+# train
+import torch.optim as optim
+# P(C|Feat)
+x_train_feature = torch.Tensor(x_test_feature)
+optimizer = optim.SGD(clf_feat.parameters(), lr=0.01, momentum=0.5)
+
+
 
 # TODO: test
 
