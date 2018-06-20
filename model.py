7f259beeb997632686db7758c12d4e14c5041479
RemiC <remicongee@gmail.com>
2018-06-20 22:01:53 +0800

    Redesign.

--- model.py
+++ model.py
@@ -1,31 +1,13 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import torch.utils.data as data
+from torch.autograd import Variable
 
 class Classifier(nn.Module):
-    def __init__(self, input_size=10, class_num = 28, mode='total', 
-                 clf_feat=None, clf_citing=None, clf_cited=None):
+    def __init__(self, input_size=10, class_num = 28):
         super(Classifier, self).__init__()
-        self.Mode = mode
-        self.ClfFeat = clf_feat
-        self.ClfCiting = clf_citing
-        self.ClfCited = clf_cited
-        self.Linear = None
-        if self.Mode != 'total':
-            self.Linear = nn.Linear(input_size, class_num)
-    
-
-    def setMode(self, mode='feature'):
-        """
-        set mode of classifier
-
-        Args:
-            mode: string type, 'feature', 'citing', 'cited' and 'total'
-        
-        Returns:
-            none
-        """
-        self.Mode = mode
+        self.Linear = nn.Linear(input_size, class_num)
 
 
     def forward(self, x):
@@ -39,9 +21,65 @@ class Classifier(nn.Module):
         Returns:
             probability of x belonging to different classes
         """
-        if self.Mode == 'total':
-            output = torch.mul(torch.mul(self.ClfFeat(x[0]), self.ClfCiting(x[1])), self.ClfCited(x[2]))
-        elif self.Mode == 'feature' or self.Mode == 'citing' or self.Mode == 'cited':
-            output = F.relu(self.Linear(x))
-            output = F.softmax(output)
+        output = F.relu(self.Linear(x))
         return output
+
+
+class PaperSet(data.Dataset):
+    def __init__(self, feature_list, label_list=None, train=True, label_dict=None):
+        self.feature_list = feature_list
+        self.label_list = list()
+        self.train = train
+        self.class_to_idx = label_dict
+        if self.train:
+            for label in label_list:
+                self.label_list.append([self.class_to_idx[label]])
+            assert len(feature_list) == len(label_list)
+
+    def __getitem__(self, idx):
+        if self.train:
+            return torch.Tensor(self.feature_list[idx]), torch.LongTensor(self.label_list[idx])
+        else:
+            return torch.Tensor(self.feature_list[idx])
+
+    def __len__(self):
+        return len(self.feature_list)
+
+
+def train(model, epoch, optimizer, features, labels, criterion, batch_size=32, label_dict=None):
+    print("--------------Make training set--------------")
+    dataset = PaperSet(feature_list=features, label_list=labels, train=True, label_dict=label_dict)
+    loader = torch.utils.data.DataLoader(dataset=dataset, batch_size=batch_size, shuffle=True)
+    print("--------------finished--------------")
+    print("--------------Start training--------------")
+    for e in range(epoch):
+        for batch_idx, (data, target) in enumerate(loader):
+            # data, target = Variable(data), Variable(target)
+            data, target = Variable(data), Variable(target.squeeze_().cuda())
+            optimizer.zero_grad()
+            output = model(data)
+            # loss
+            loss = criterion(output, target)
+            loss.backward()
+            # update
+            optimizer.step()
+            if batch_idx % 200 == 0:
+                print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                    e, batch_idx * len(data), len(loader.dataset),
+                    100. * batch_idx / len(loader), loss.data[0]))
+
+
+def test(model, features, batch_size=32):
+    print("--------------Make test set--------------")
+    dataset = PaperSet(feature_list=features, train=False)
+    loader = torch.utils.data.DataLoader(dataset=dataset, batch_size=1, shuffle=False)
+    print("--------------finished--------------")
+    print("--------------Start testing--------------")
+    model.eval()
+    pred_list = list()
+    for feature in loader:
+        output = model(Variable(feature))
+        output = F.softmax(output)
+        pred_list.append(output.cpu().data.numpy())
+    return pred_list
+
--- run.py
+++ run.py
@@ -1,68 +1,153 @@
-from model import Classifier
 from utils import *
-import torch
+from model import *
 
 
-# path initialization
-graph_cite_path = 'Data/Cit-HepTh.txt'
-graph_cited_path = 'Data/Cited-HepTh.txt'
+filename = 'Data/trainText_nltk.txt'
+train_text = list()
+with open(filename, 'r') as file_to_read:
+    lines = file_to_read.readlines()
+    for line in lines:
+        train_text.append(line)
+
+filename = 'Data/testText_nltk.txt'
+test_text = list()
+with open(filename, 'r') as file_to_read:
+    lines = file_to_read.readlines()
+    for line in lines:
+        test_text.append(line)
+
+text_train, extractor = extractFeature(train_text)
+text_test = extractor.transform(test_text).toarray()
+
 train_path = 'Data/train.csv'
 test_path = 'Data/test.csv'
-info_path = 'Data/node_information'
+train_ids, y_train = readIdLabel(train_path)
+unique = np.unique(y_train).tolist()
+index = range(len(unique))
+index2label = dict(zip(index, unique))
+label2index = dict(zip(unique, index))
+test_ids, _ = readIdLabel(test_path)
 
-# construct graph dataframe
-graph_cite = readGraph(graph_cite_path)
-graph_cited = readGraph(graph_cited_path)
-
-# read node information
+graph_cite_path = 'Data/Cit-HepTh.txt'
+info_path = 'Data/node_information.csv'
 info_df = readInfo(info_path)
 
-# read training set, make training graph
-x_train_id, y_train_label = readIdLabel(train_path)
-graph_cite_train = graphTrain(graph_cite, x_train_id)
-graph_cited_train = graphTrain(graph_cited, x_train_id)
+n_train = len(train_ids)
+G = readGraph(graph_cite_path)
+UG = nx.Graph(G)
+nxc = nx.clustering(UG)
+avg_neig_deg_in = nx.average_neighbor_degree(G, nodes=train_ids, source='in', target='in')
+avg_neig_deg_out = nx.average_neighbor_degree(G, nodes=train_ids, source='out', target='out')
+graph_train = np.zeros((n_train, 6))
+for i in range(n_train):
+	degree_abs = G.out_degree(train_ids[i]) + G.in_degree(train_ids[i]) + 1e-6
+	graph_train[i,0] = G.out_degree(train_ids[i]) / degree_abs
+	graph_train[i,1] = G.in_degree(train_ids[i]) / degree_abs
+	degree_abs = avg_neig_deg_in[train_ids[i]] + avg_neig_deg_out[train_ids[i]] + 1e-6
+	graph_train[i,2] = avg_neig_deg_in[train_ids[i]] / degree_abs
+	graph_train[i,3] = avg_neig_deg_out[train_ids[i]] / degree_abs
+	graph_train[i,4] = nxc[train_ids[i]] * 10
+	graph_train[i,5] = (float(info_df.loc[info_df['id'] == int(train_ids[i])]['year'].iloc[0]) - 1992.0) / (2000.0 - 1992.0)
 
-# transform label to one-hot vector
-y_train_onehot = label2onehot(y_train_label)
+n_test = len(test_ids)
+avg_neig_deg_in = nx.average_neighbor_degree(G, nodes=test_ids, source='in', target='in')
+avg_neig_deg_out = nx.average_neighbor_degree(G, nodes=test_ids, source='out', target='out')
+graph_test = np.zeros((n_test, 6))
+for i in range(n_test):
+	degree_abs = G.out_degree(test_ids[i]) + G.in_degree(test_ids[i]) + 1e-6
+	graph_test[i,0] = G.out_degree(test_ids[i]) / degree_abs
+	graph_test[i,1] = G.in_degree(test_ids[i]) / degree_abs
+	degree_abs = avg_neig_deg_in[test_ids[i]] + avg_neig_deg_out[test_ids[i]] + 1e-6
+	graph_test[i,2] = avg_neig_deg_in[test_ids[i]] / degree_abs
+	graph_test[i,3] = avg_neig_deg_out[test_ids[i]] / degree_abs
+	graph_test[i,4] = nxc[test_ids[i]] * 10
+	graph_test[i,5] = (float(info_df.loc[info_df['id'] == int(test_ids[i])]['year'].iloc[0]) - 1992.0) / (2000.0 - 1992.0)
 
-# read test set
-x_test_id, _ = readIdLabel(test_path)
+cite_train = np.zeros((n_train, len(unique)))
+for i, id in enumerate(train_ids):
+    for node in G.successors(id):
+        if node in train_ids:
+            node = y_train[train_ids.index(node)]
+            cite_train[i][label2index[node]] += 1
 
-# read text
-x_train_text = readText(info_df, x_train_id)
-x_test_text = readText(info_df, x_test_id)
+cite_test = np.zeros((n_test, len(unique)))
+for i, id in enumerate(test_ids):
+    for node in G.successors(id):
+        if node in train_ids:
+            node = y_train[train_ids.index(node)]
+            cite_test[i][label2index[node]] += 1
 
-# extract features, make graph featured
-x_train_feature, extractor = extractFeature(x_train_text)
-x_test_feature = extractor.transform(x_test_text)
+graph_cited_path = 'Data/Cited-HepTh.txt'
+GG = readGraph(graph_cited_path)
+cited_train = np.zeros((n_train, len(unique)))
+for i, id in enumerate(train_ids):
+    for node in GG.successors(id):
+        if node in train_ids:
+            node = y_train[train_ids.index(node)]
+            cited_train[i][label2index[node]] += 1
 
-# make citation feature
-# for training set
-x_train_cite = combineFeature(graph_cite_train, x_train_id, x_train_feature)
-x_train_cited = combineFeature(graph_cited_train, x_train_id, x_train_feature)
-# for whole data
-x_test_cite = combineFeature(graph_cite, x_test_id, x_test_feature)
-x_test_cited = combineFeature(graph_cited, x_test_id, x_test_feature)
+cited_test = np.zeros((n_test, len(unique)))
+for i, id in enumerate(test_ids):
+    for node in GG.successors(id):
+        if node in train_ids:
+            node = y_train[train_ids.index(node)]
+            cited_test[i][label2index[node]] += 1
 
-# initialization
-feature_size = len(x_train_cite[0])
-clf_feat = Classifier(input_size=feature_size, mode='feature')
-clf_cite = Classifier(input_size=feature_size, mode='cite')
-clf_cited = Classifier(input_size=feature_size, mode='cited')
-clf = Classifier(input_size=feature_size, mode='total',
-                 clf_feat=clf_feat, clf_cite=clf_cite, clf_cited=clf_cited)
+x_train = list()
+for i in range(n_train):
+    feature_train = np.hstack((cite_train[i] + cited_train[i], graph_train[i], text_train[i]))    
+    x_train.append(feature_train)
 
-# train
-import torch.optim as optim
-# P(C|Feat)
-x_train_feature = torch.Tensor(x_test_feature)
-optimizer = optim.SGD(clf_feat.parameters(), lr=0.01, momentum=0.5)
+x_test = list()
+for i in range(n_test):
+    feature_test = np.hstack((cite_test[i] + cited_test[i], graph_test[i], text_test[i]))
+    x_test.append(feature_test)
 
+from sklearn.linear_model import LogisticRegression, LogisticRegressionCV, SGDClassifier
+clf = LogisticRegression(max_iter=1000, tol=1e-4, C=2.0, penalty='l2')
+clf.fit(x_train, y_train)
 
+y_pred = clf.predict_proba(x_test)
+# Write predictions to a file
+with open('Result/submission.csv', 'w') as csvfile:
+    writer = csv.writer(csvfile, delimiter=',')
+    lst = clf.classes_.tolist()
+    lst.insert(0, "Article")
+    writer.writerow(lst)
+    for i,test_id in enumerate(test_ids):
+        lst = y_pred[i,:].tolist()
+        lst.insert(0, test_id)
+        writer.writerow(lst)
 
-# TODO: test
+''' 
+####### Pytorch Implementation ######
+input_size = len(x_train[0])
+epoch = int(50)
+learning_rate = 0.001
 
+clf = Classifier(input_size=input_size)
+clf = torch.nn.DataParallel(clf).cuda()
+print(clf)
 
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(clf.parameters(), lr=learning_rate)
 
+train(model=clf, epoch=epoch, optimizer=optimizer, features=x_train, labels=y_train, criterion=criterion, label_dict=label2index)
+
+y_pred = test(model=clf, features=x_test)
+y_pred = np.reshape(np.array(y_pred), (-1, len(unique)))
+
+np.save("Result/Submission.npy", y_pred)
+
+# Write predictions to a file
+with open('Result/submission.csv', 'w') as csvfile:
+    writer = csv.writer(csvfile, delimiter=',')
+    unique.insert(0, "Article")
+    writer.writerow(unique)
+    for i,test_id in enumerate(test_ids):
+        lst = y_pred[i].tolist()
+        lst.insert(0, test_id)
+        writer.writerow(lst)
+'''
 
 
